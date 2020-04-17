0ea6323409bbc3bc0c40dc0f2517349b6be6ab1a
khang <khangtn50@gmail.com>
2020-04-16 19:01:06 -0500

    commit

--- backend/routes/user-route.js
+++ backend/routes/user-route.js
@@ -6,11 +6,8 @@ const router = express.Router();
 router.post("/login", UserController.userLogin); 
 //router.post("reset-req", UserController.userResetReq);
 
-<<<<<<< HEAD
-=======
 // route to user-controller.js
 router.get("/getUser", UserController.getUser);   
 router.post("/addUser", UserController.addUser);
 
->>>>>>> khang
 module.exports = router;
